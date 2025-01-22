afeae00971351b51a82e89d64c6bf2c26c501855
Shruti Rai <shruti.rai2901@gmail.com>
2025-01-22 11:15:32 +0530

    update

--- conditionals/calculator_switch.cpp
+++ conditionals/calculator_switch.cpp
@@ -3,38 +3,42 @@ using namespace std;
 
 int main(){
     int a, b;
-    char op;
+    char op, permission;
     cout << "Enter 2 numbers : ";
     cin >> a >> b;
     cout << "Enter the operator : ";
     cin >> op;
 
-    // if(op == '+'){
-    //     cout << "The sum is : " << a + b << endl;
-    // }else if(op == '-'){
-    //     cout << "The difference is : " << a - b << endl;
-    // }else if(op == '*'){
-    //     cout << "The product is : " << a * b << endl;
-    // }else if(op == '/'){
-    //     cout << "The quotient is : " << a / b << endl;
-    // }else{
-    //     cout << "Invalid operator" << endl;
-    // }
-    switch(op){
-        case '+':
-            cout << "The sum is : " << a + b << endl;
-            break;
-        case '-':
-            cout << "The difference is : " << a - b << endl;
-            break;
-        case '*':
-            cout << "The product is : " << a * b << endl;
-            break;
-        case '/':
-            cout << "The quotient is : " << a / b << endl;
-            break;
-        default:
-            cout << "Invalid operator" << endl;
+    do{
+        // if(op == '+'){
+        //     cout << "The sum is : " << a + b << endl;
+        // }else if(op == '-'){
+        //     cout << "The difference is : " << a - b << endl;
+        // }else if(op == '*'){
+        //     cout << "The product is : " << a * b << endl;
+        // }else if(op == '/'){
+        //     cout << "The quotient is : " << a / b << endl;
+        // }else{
+        //     cout << "Invalid operator" << endl;
+        // }
+        switch(op){
+            case '+':
+                cout << "The sum is : " << a + b << endl;
+                break;
+            case '-':
+                cout << "The difference is : " << a - b << endl;
+                break;
+            case '*':
+                cout << "The product is : " << a * b << endl;
+                break;
+            case '/':
+                cout << "The quotient is : " << a / b << endl;
+                break;
+            default:
+                cout << "Invalid operator" << endl;
+        }
+        cout << "Do you want to continue(y / n): ";
+        cin >> permission;
     }
-
+    while(permission == 'y');
 }
